b6537bc4e11798c26703955e1cb0d37ed49aef21
K Samarth N Kamath <kamathsamarth@gmail.com>
2022-05-03 13:06:53 +0530

    Assets Table

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 import Header from '../components/Header'
+import Main from '../components/Main'
 
 const Dashboard = ({address}) => {
   return (
-    <Header address={address}/>
+    <Wrapper>
+      <MainContainer>
+        <Header address={address}/>
+        <Main />
+      </MainContainer>
+    </Wrapper>
   )
 }
 
 export default Dashboard
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vh;
+  background-color: #0a0b0d;
+  color: white;
+`
+const MainContainer = styled.div`
+  flex: 1;
+`
