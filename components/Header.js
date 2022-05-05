1c276e78b3eb0519e03e691d12fb66df68eb3e5c
Samarth <samarth.cs18@sahyadri.edu.in>
2022-05-05 15:10:29 +0530

    Modal

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import Modal from 'react-modal'
+import { useRouter } from 'next/router'
+import TransferModal from './modal/TransferModal'
+import Link from 'next/link'
+
+//Modal.setAppElement('#_next');
 
 const Header = ({walletAddress, connectWallet}) => {
+  const router = useRouter()
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: '#0a0b0d',
+      padding: 0,
+      border: 'none',
+    },
+    overlay:{
+      backgroundColor: 'rgba(10,11,13,0.75)',
+    },
+  }
+  
   return (
     <Wrapper>
       <Title>
@@ -17,11 +41,19 @@ const Header = ({walletAddress, connectWallet}) => {
         <Button style={{ backgroundColor: '#3773f5', color: '#000' }}>
           Buy/Sell
         </Button>
-        <Button>
-          Send/Receive
-        </Button>
-        
+        <Link href={'/?transfer=1'}>
+          <Button>
+            Send/Receive
+          </Button>
+        </Link>
       </ButtonsContainer>
+      <Modal 
+      isOpen={router.query.transfer}
+      onRequestClose={()=>router.push('/')}
+      style={customStyles}
+      >
+        <TransferModal/>
+      </Modal>
     </Wrapper>
     
 
@@ -61,8 +93,8 @@ const WalletLink = styled.div`
   border: 1px solid #282b2f;
   border-radius: 50rem;
   font-size: 1.2rem;
-  margin-right: 1 rem;
-  padding: 0 1 rem;
+  margin-right: 1rem;
+  padding: 0rem 1rem;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
